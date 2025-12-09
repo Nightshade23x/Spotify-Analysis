a439a57c07318a5aea0b44c3663054925967f440
Nightshade23x <samarmahajan23@gmail.com>
2025-12-09 16:44:12 +0200

    window func

--- analysis.sql
+++ analysis.sql
@@ -1,3 +1,26 @@
-select  DISTINCT track_name,artists,popularity
+
+
+--intermediate analysis
+select  DISTINCT track_name, artists, popularity
 from spotify_tracks
-order by popularity desc
+where popularity > 90
+and danceability > 0.8
+and explicit = 'False'
+order by popularity desc;
+
+--window func
+SELECT *
+FROM (
+    SELECT
+        track_name,
+        artists,
+        track_genre,       -- ADD THIS
+        popularity,
+        row_number() OVER (
+            PARTITION BY track_genre
+            ORDER BY popularity DESC
+        ) AS genre_rank
+    FROM spotify_tracks
+) ranked
+WHERE genre_rank <= 3
+ORDER BY track_genre, genre_rank;
